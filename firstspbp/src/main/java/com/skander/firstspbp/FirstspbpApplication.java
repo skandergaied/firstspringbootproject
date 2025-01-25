d10e0cdc2cce69fd71992b12d4be96fd40033a54
Iskandar <73014102+skandergaied@users.noreply.github.com>
2025-01-26 02:31:42 +0300

    initialized spring boot project

---
+++ firstspbp/src/main/java/com/skander/firstspbp/FirstspbpApplication.java
@@ -0,0 +1,13 @@
+package com.skander.firstspbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstspbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstspbpApplication.class, args);
+	}
+
+}
---
+++ firstspbp/src/test/java/com/skander/firstspbp/FirstspbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skander.firstspbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
